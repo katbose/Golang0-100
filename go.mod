d190607865853e960c1e4b8914b8c63549ee854e
Katabathuni Bose <53533409+katbose@users.noreply.github.com>
2026-09-11 09:20:40 +0000

    Add initial Go module and example files
    - Created go.mod for module management
    - Added hello-world.go with a simple hello function
    - Added main.go as the entry point of the program
    - Added math.go to demonstrate random number generation
    - Included hello-world.exe as a compiled binary

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/katbose
+
+go 1.27.0
---
+++ math.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+    "fmt"
+    "math/rand"
+)
+
+func math() {
+    fmt.Println("My fav no. is:", rand.Intn(10))
+}
